97b4707970dd5b9bae2c4ee00fb56a2a09de3a51
Alexey Bystrov <strikeentco@gmail.com>
2016-03-04 18:08:33 +0300

    Minor grammar edits

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ function * generator() {
 	yield 3;
 }
 
-test('get iterable as a array', t => {
+test('get an iterable as an array', t => {
 	t.same(fn([1, 2, 3]), [1, 2, 3]);
 	t.same(fn(generator()), [1, 2, 3]);
 	t.end();
